c84532c838b7be29109af93a0ae4a1c9b162ca12
louise.kirkham <louise.kirkham@royalmail.com>
2020-06-09 10:29:31 +0000

    add code in files types: .py remove code in files types:

---
+++ content.py
@@ -0,0 +1,17 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo("/Users/louisekirkham/Documents/projects/production_scheduler/demand-forecast")
+
+# Your mock repo
+mock_repo = git.Repo("/Users/louisekirkham/Documents/my_projects/mock_repo_rm_ps")
+importer = Importer([repo], mock_repo)
+
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github profiles uses
+# my work email
+importer.set_author(['louisek@gmail.com', 'louise.kirkham@royalmail.com'])
+importer.import_repository()
+print("qshmd")
