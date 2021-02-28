81c4e61e3e5396d28161e78449b576101060e35d
Louise Kirkham <louise.kirkham@royalmail.com>
2021-02-28 17:47:50 +0000

    Updated paths.

--- run_script.py
+++ run_script.py
@@ -7,11 +7,13 @@ repo = git.Repo("/Users/louisekirkham/Documents/Projects/demand-forecast")
 
 # Your mock repo
 mock_repo = git.Repo("/Users/louisekirkham/Documents/Projects/github_mock_repos/mock_repo_rm_ps")
+
 importer = Importer([repo], mock_repo)
 
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github profiles uses
 # my work email
 importer.set_author(['louisek@gmail.com', 'louise.kirkham@royalmail.com'])
+
 importer.import_repository()
 
